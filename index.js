423071bb64abe506e9491544a68216fb87f228eb
Greg Stimson <stimsong@yahoo.com>
2018-02-03 01:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,21 +57,23 @@ function createRock(x) {
   rock.className = 'rock'
   rock.style.left = `${x}px`
 
-  // Hmmm, why would we have used `var` here?
+           // Hmmm, why would we have used `var` here?
   var top = 0
 
   rock.style.top = top
 
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
+           /**
+            * Now that we have a rock, we'll need to append
+            * it to GAME and move it downwards.
+            */
+   
+  GAME.appendChild(rock);
 
-
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
+           /**
+            * This function moves the rock. (2 pixels at a time
+            * seems like a good pace.)
+            */
+            
   function moveRock() {
     // implement me!
     // (use the comments below to guide you!)
@@ -127,6 +129,7 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   
 }
 
 function moveDodgerRight() {
